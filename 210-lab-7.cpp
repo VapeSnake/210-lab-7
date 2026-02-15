fe1fc5e156a1e957b694d1a6724164aeac26aa48
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-14 21:37:42 -0800

    5th Commit

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -14,16 +14,29 @@ main()
     string *arrayPtr = nullptr; // Creates our pointer for our dynamic string array
     arrayPtr = new string[SIZE]{"Bill", "Javier", "Arthur", "John", "Dutch"};
 
+    cout << "Original array: ";
+    displayArray(arrayPtr, SIZE) << endl;
+    arrayPtr = reverseArray(arrayPtr, SIZE);
+
     delete[] arrayPtr; // Always release memory when done.
     return 0;
 }
 
-string* reverseArray(string *ptr, int size)
+string *reverseArray(string *ptr, int size)
 {
-    for (int i = 0; i < size; i++) //Loop should only happen 2(?) times. Any more will mess with reverse function.
-    { 
-        string temp = *(ptr + i); // Create temp to begin reverse loop
+    for (int i = 0; i < size / 2; i++) // Loop should only happen 2(?) times. Any more will
+    {
+        string temp = *(ptr + i);             // Create temp to begin reverse loop
         *(ptr + i) = *(ptr + (size - 1 - i)); // Each element will swap from outside-to-inside
         *(ptr + (size - 1 - i)) = temp;
+    }
+    return ptr;
+}
+
+void displayArray(string *ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(ptr + i) << " ";
     }
 }
