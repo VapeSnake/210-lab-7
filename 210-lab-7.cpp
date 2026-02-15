8453c63c8d9e1ea06b797ed3df879a36cfe1599e
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-14 21:22:57 -0800

    4th Commit

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -1,19 +1,29 @@
-//COMSC-210|Lab 7|Noel Mier-Luna
+// COMSC-210|Lab 7|Noel Mier-Luna
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-const int SIZE = 5; //Keep those magic numbers out of Diablo Valley College.
+const int SIZE = 5; // Keep those magic numbers out of Diablo Valley College.
 
-string* reverseArray(string*, int SIZE);
-void displayArray(string*, int SIZE);
+string *reverseArray(string *, int SIZE);
+void displayArray(string *, int SIZE);
 
 main()
 {
     string *arrayPtr = nullptr; // Creates our pointer for our dynamic string array
     arrayPtr = new string[SIZE]{"Bill", "Javier", "Arthur", "John", "Dutch"};
 
-    
+    delete[] arrayPtr; // Always release memory when done.
     return 0;
+}
+
+string* reverseArray(string *ptr, int size)
+{
+    for (int i = 0; i < size; i++) //Loop should only happen 2(?) times. Any more will mess with reverse function.
+    {
+        string temp = *(ptr + i); // Create temp to begin reverse loop
+        *(ptr + i) = *(ptr + (size - 1 - i)); // Each element will swap from outside-to-inside
+        *(ptr + (size - 1 - i)) = temp;
+    }
 }
