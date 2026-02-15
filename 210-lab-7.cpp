e114fbb3d691acc221d8824ab616a47f5dd15fe1
Noel Mier-Luna <noel.lmier@gmail.com>
2026-02-14 21:53:02 -0800

    Final commit

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -9,10 +9,15 @@ const int SIZE = 5; // Keep those magic numbers out of Diablo Valley College.
 string *reverseArray(string *, int SIZE);
 void displayArray(const string *, int SIZE);
 
-int main()
-{
-    string *arrayPtr = nullptr; // Creates our pointer for our dynamic string array
-    arrayPtr = new string[SIZE]{"Bill", "Javier", "Arthur", "John", "Dutch"};
+int main() {
+    string * arrayPtr = nullptr; // Creates our pointer for our dynamic string array
+    arrayPtr = new string[SIZE] {
+        "Bill",
+        "Javier",
+        "Arthur",
+        "John",
+        "Dutch"
+    };
 
     cout << "Original array: ";
     displayArray(arrayPtr, SIZE);
@@ -23,21 +28,19 @@ int main()
     return 0;
 }
 
-string *reverseArray(string *ptr, int size)
-{
+string * reverseArray(string * ptr, int size) {
     for (int i = 0; i < size / 2; i++) // Loop should only happen 2 times. Our middle element will stay the same.
     {
-        string temp = *(ptr + i);             // Create temp to begin reverse loop as first element
-        *(ptr + i) = *(ptr + (size - 1 - i)); // Each element will swap from outside-to-inside
-        *(ptr + (size - 1 - i)) = temp; 
+        string temp = * (ptr + i); // Create temp to begin reverse loop as first element
+        *(ptr + i) = * (ptr + (size - 1 - i)); // Each element will swap from outside-to-inside
+        *(ptr + (size - 1 - i)) = temp;
     }
     return ptr;
 }
 
-void displayArray(const string *ptr, int size) // Nothing gets modified so ptr is constant.
+void displayArray(const string * ptr, int size) // Nothing gets modified so ptr is constant.
 {
-    for (int i = 0; i < size; i++)
-    {
-        cout << *(ptr + i) << " ";
+    for (int i = 0; i < size; i++) {
+        cout << * (ptr + i) << " ";
     }
 }
